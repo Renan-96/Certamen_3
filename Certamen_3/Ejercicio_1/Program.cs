6d0d11168255d4f60645212b65eb0a28f38a65b4
Natanael <namuji38@gmail.com>
2020-08-14 22:57:21 -0400

    Comienzo ejercicio1 Matriz
    forma una L al revez

--- Certamen_3/Ejercicio_1/Program.cs
+++ Certamen_3/Ejercicio_1/Program.cs
@@ -10,6 +10,40 @@ namespace Ejercicio_1
     {
         static void Main(string[] args)
         {
+            int Y=0,X=0;
+            Console.Write("MATRIZ X: ");
+            X=int.Parse(Console.ReadLine());
+            int[] Vector = new int[X];
+
+            Console.Write("MATRIZ Y: ");
+            Y = int.Parse(Console.ReadLine());         
+            int[] Vector2 = new int[Y];
+
+            for (int i = 0; i < Vector.Length; i++)
+            {           
+                Console.Write("1");
+                for (int j = 0; j < Vector2.Length; j++)
+                {
+                    if (i==0||i>Vector.Length)
+                    {
+                        Console.Write("1");
+                    }
+                    else if (i)
+                    {
+                        Console.Write("1");
+                    }                    
+                    else
+                    {
+                        Console.Write("0");
+                    }
+                    
+
+                }
+                Console.WriteLine("");
+                
+            }
+            Console.ReadKey();
         }
+       
     }
 }
