f913f56cfd7eeee5704d099159a7be5886986268
Natanael <namuji38@gmail.com>
2020-08-20 23:42:51 -0400

    Base 2 y 3
    Te deje lista el void para que inicies

--- Certamen_3/Ejercicio_1/Program.cs
+++ Certamen_3/Ejercicio_1/Program.cs
@@ -10,24 +10,31 @@ namespace Ejercicio_1
     {
         static void Main(string[] args)
         {
-            //Aqui matri1 cuadrado perfecto
+            Console.WriteLine("Ejercicio1");
             Console.WriteLine("MATRIZ 1 ");
-            Console.WriteLine();
             matriz1();
             Console.WriteLine();
             Console.WriteLine("MATRIZ 2 ");
-            Console.WriteLine();
             matriz2();
+            Console.WriteLine();
+            Console.WriteLine("MATRIZ 3 ");
+            matriz3();
+            Console.WriteLine();
+            Console.WriteLine("MATRIZ 4 ");
+            matriz4();
+            Console.WriteLine("Ejercicio2");
+            ejercicio_2();
         }
-        static void matriz1() {
+
+        static void matriz1()
+        {
             int Y = 0, X = 0;
-            Console.Write("MATRIZ X: ");
+            Console.Write("MATRIZ Y: ");
             X = int.Parse(Console.ReadLine());
             int[] Vector = new int[X];
-            Console.Write("MATRIZ Y: ");
+            Console.Write("MATRIZ X: ");
             Y = int.Parse(Console.ReadLine());
             int[] Vector2 = new int[Y];
-
             for (int i = 1; i <= Vector.Length; i++)//Y
             {
                 if (i == Vector.Length)
@@ -59,18 +66,135 @@ namespace Ejercicio_1
             Console.WriteLine();
         }
 
-        static void matriz2()
-        {
-            
-            Console.Write("Ingrese X: ");
-            Console.Write("Ingrese Y: ");
+        static void matriz2() {
+            int Y = 0, X = 0;
+            Console.Write("MATRIZ Y: ");
+            X = int.Parse(Console.ReadLine());
+            Console.Write("MATRIZ X: ");
+            Y = int.Parse(Console.ReadLine());
 
 
+            int[,] vec = new int[X, Y];
+            int cantidad = vec.GetLength(1) - 1;
 
-
+            for (int i = 0; i < vec.GetLength(0); i++)
+            {
+                for (int j = 0; j < vec.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        vec[i, j] = 1;
+                    }
+                    else
+                    {
+                        if (i == cantidad)
+                        {
+                            vec[i, j] = 1;
+                        }
+                        else
+                        {
+                            vec[i, j] = 0;
+                        }
+                    }
+                    Console.Write(vec[i, j]);
+                    cantidad--;
+                }
+                Console.WriteLine("");
+                cantidad = vec.GetLength(1) - 1;
+            }
             Console.ReadKey();
-        }    
-        
-        
+        }
+
+        static void matriz3()
+        {
+            int Y = 0, X = 0;
+            Console.Write("MATRIZ Y: ");
+            X = int.Parse(Console.ReadLine());
+            int[] Vector = new int[X];
+            Console.Write("MATRIZ X: ");
+            Y = int.Parse(Console.ReadLine());
+            int[] Vector2 = new int[Y];
+            for (int i = 1; i <= Vector.Length; i++)
+            {
+                for (int j = 1; j <= Vector2.Length; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write("0");
+                    }
+                    else
+                    {
+                        Console.Write("1");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+        static void matriz4()
+        {
+            int Y = 0, X = 0;
+            Console.Write("MATRIZ Y: ");
+            X = int.Parse(Console.ReadLine());
+            int[] Vector = new int[X];
+            Console.Write("MATRIZ X: ");
+            Y = int.Parse(Console.ReadLine());
+            int[] Vector2 = new int[Y];
+            for (int i = 1; i <= Vector.Length; i++)
+            {
+                for (int j = 1; j <= Vector2.Length; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        Console.Write("0");
+                    }
+                    else
+                    {
+                        Console.Write("1");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+        static void ejercicio_2()
+        {
+            int numM = 0;
+            Console.Write("Ingrese cantidad de numeros :");
+            numM = int.Parse(Console.ReadLine());
+            int[] Vector = new int[numM];
+            for (int i = 0; i < Vector.Length; i++)
+            {
+                Console.Write("Numero de la posicion {0}: ", (i + 1));
+                Vector[i] = int.Parse(Console.ReadLine());
+            }
+            for (int j = 0; j < Vector.Length; j++)
+            {
+                if (j == 0)
+                {
+                    Console.WriteLine("El numero menor es: {0}", Vector[j]);
+                }
+                if (j == (Vector.Length - 1))
+                {
+                    Console.WriteLine("el numero mayor es: {0}", Vector[j]);
+                }
+            }
+            Console.ReadKey();
+        }
+
+        static void salida(){
+
+
+
+        }
+
+        static void ejercicio_3()
+        {
+            //Aqui va la parte de renan
+        }
+
+
     }
 }
--- Certamen_3/Ejercicio_2/Program.cs
+++ Certamen_3/Ejercicio_2/Program.cs
@@ -10,6 +10,42 @@ namespace Ejercicio_2
     {
         static void Main(string[] args)
         {
+            //Cantidad de homrbes y mujeres, mayores y menores de edad
+            Console.Write("Cantidad de registros :");
+            int registros = int.Parse(Console.ReadLine());
+            int[] Valumnos= new int[registros];
+            int contadorm = 0,contadorf=0;
+            for (int i = 0; i < Valumnos.Length; i++)
+            {
+                bool bucle = true;
+                string sexo;
+                while (bucle == true)
+                {
+                    Console.Write("M = Masculino, F = Femenino  ");
+                    Console.Write("Ingrese su sexo: ");
+                    sexo = Console.ReadLine();
+
+                    if (sexo == "m" || sexo == "f" || sexo == "F" || sexo == "M")
+                    {
+                        if (sexo=="m"||sexo=="M")
+                        {
+                            contadorm++;
+                        }
+                        if (sexo == "f" || sexo == "F")
+                        {
+                            contadorf++;
+                        }
+                        bucle = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Intente nuevamente solo se permite M o F");
+                    }
+                }
+            }
+            Console.WriteLine("{0} hombres hay en el curso",contadorm);
+            Console.WriteLine("{0} mujeres hay en el curso",contadorf);
+            Console.ReadKey();
         }
     }
 }
